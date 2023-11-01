b8bfd024817efd264a04de8cd0417574913bb0a8
Rafael Ribeiro <Rafaelribeiro12@live.com>
2023-11-01 18:50:16 -0300

    Ajustado pagina inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,11 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="grid grid-cols-1 gap-2 h-[700px]">
-      <div className="bg-sky-500 justify-center items-center">
-        <main className="text-center">
-          <h1 className="text-3x1 text-slate-300">Home root</h1>
-          Escolha um dos topicos na navbar
+      <div className="bg-sky-200 justify-center items-center">
+        <main className=" m-4 text-center">
+          <h1 className="text-5xl text-slate-700">Bem vindo!</h1>
+          Base de dados criada para armazenar informações aprendidas ao longo do
+          tempo, e também poder compartilhar informações com outros devs.
         </main>
       </div>
     </div>
