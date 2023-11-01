0e350edec091295d358e4d53cb00a3a548f87fa1
Rafael Ribeiro <Rafaelribeiro12@live.com>
2023-11-01 11:03:43 -0300

    Salvando ultimo estado estável e funcional da pagina

---
+++ src/app/html/nav/page.tsx
@@ -0,0 +1,12 @@
+export default function Nav() {
+  return (
+    <>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi animi
+        porro esse voluptates? Neque, animi labore, ullam officia maxime quaerat
+        reiciendis ea, numquam nostrum explicabo obcaecati nesciunt voluptas
+        distinctio optio?
+      </p>
+    </>
+  );
+}
